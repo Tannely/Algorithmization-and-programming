89b60eae987ba6e5e8f8a6a6243a16025095aa45
Tannely <taniakorchaga@gmail.com>
2021-11-11 11:20:02 +0200

    created BinTree

--- BinTree/BinaryTree.cpp
+++ BinTree/BinaryTree.cpp
@@ -2,18 +2,18 @@
 #include <iostream>
 using namespace std;
 
-int tabs = 0; //для отступів
-int amount = 0; // для парного
-int amount_1 = 1; // для непарного
+int tabs = 0; //dlya otstupiv
+int amount = 0; // dlya parnoho
+int amount_1 = 0; // dlya neparnoho
 struct Branch
 {
-	int Data; // поле даник
+	int Data;// pole danyh
 	Branch* RightBranch;
 	Branch* LeftBranch; 
 	
 };
 
-void Add(int aData, Branch*& aBranch)// функція додавання елементів до бінарного дерева
+void Add(int aData, Branch*& aBranch)// funktsiya dodavannya elementiv do binarnoho dereva
 {
 	if (!aBranch)
 	{ 
@@ -25,50 +25,50 @@ void Add(int aData, Branch*& aBranch)// функція додавання елементів до бінарного
 	}
 	else 
 		if (aBranch->Data > aData)
-		{ //Якщо воно менше того, що в цій гілці - додамо вліво
+		{ //Yakshcho vono menshe toho, shcho v tsiy hiltsi - dodamo vlivo
 			Add(aData, aBranch->LeftBranch);
 		}
 		else
-		{ // інакше в праву гілку
+		{ // inakshe v pravu hilku
 			Add(aData, aBranch->RightBranch);
 		};
 }
 
-void print(Branch* aBranch) // функція виведення дерева
+void print(Branch* aBranch) // funktsiya vyvedennya dereva
 {
-	if (!aBranch) return; //Якщо гілки не існує – виходимо. Виводити нічого.
-	tabs += 5; //Інакше збільшимо лічильник рекурсивно викликаних процедур
-	//Який рахуватиме відступи для виведення на екран
+	if (!aBranch) return; //Yakshcho hilky ne isnuye – vykhodymo. Vyvodyty nichoho.
+	tabs += 5; //Inakshe zbilshymo lichylnyk rekursyvno vyklykanykh protsedur
+    //Yakyy rakhuvatyme vidstupy dlya vyvedennya na ekran
 	print(aBranch->LeftBranch); 
 	for (int i = 0; i < tabs; i++) cout << " "; 
 	cout << aBranch->Data << endl; 
 
 	print(aBranch->RightBranch);
-	tabs-= 5; //після зменшення кількості відступів
+	tabs-= 5; //pislya zmenshennya kil?kosti vidstupiv
 	return;
 }
 
-void pr_obh(Branch*& aBranch) // прямий обхід
+void pr_obh(Branch*& aBranch) // pryamyy obkhid
 {
-	if (NULL == aBranch)    return;    //Якщо дерева немає, виходимо
+	if (NULL == aBranch)    return;    //Yakshcho dereva nemaye, vykhodymo
 
-	cout << aBranch->Data << endl; //вузол дерева
-	pr_obh(aBranch->LeftBranch); //обхід лівого піддерева 
-	pr_obh(aBranch->RightBranch); //обхід правого піддерева 
+	cout << aBranch->Data << endl; //vuzol dereva
+	pr_obh(aBranch->LeftBranch); //obkhid livoho piddereva
+	pr_obh(aBranch->RightBranch); //obkhid pravoho piddereva 
 }
 
-void gor_obh(Branch*& aBranch) // горизонтальний(симетричний) обхід
+void gor_obh(Branch*& aBranch) horyzontalnyy(symetrychnyy) obkhid
 {
-	if (NULL == aBranch)    return;    //Якщо дерева немає, виходимо
+	if (NULL == aBranch)    return;    //Yakshcho dereva nemaye, vykhodymo
 
-	gor_obh(aBranch->LeftBranch); //обхід лівого піддерева 
-	cout << aBranch->Data << endl; //вузол дерева
-	gor_obh(aBranch->RightBranch); //обхід правого піддерева 
+	gor_obh(aBranch->LeftBranch); //obkhid livoho piddereva
+	cout << aBranch->Data << endl; //vuzol dereva
+	gor_obh(aBranch->RightBranch);  //obkhid pravoho piddereva
 }
 
-int par(Branch*& aBranch) // для парних елементів
+int par(Branch*& aBranch) // dlya parnykh elementiv
 {
-	if (NULL == aBranch)    return 0;    //Якщо дерева немає, виходимо
+	if (NULL == aBranch)    return 0;    //Yakshcho dereva nemaye, vykhodymo
 
 	if (aBranch->Data % 2 == 0)
 	{
@@ -78,7 +78,7 @@ int par(Branch*& aBranch) // для парних елементів
 	par(aBranch->RightBranch); 
 	return amount;
 }
-int ne_par(Branch*& aBranch)   // для непарних елементів
+int ne_par(Branch*& aBranch)   // dlya neparnykh elementiv
 {
 	if (NULL == aBranch)    return 0;   
 
@@ -93,7 +93,7 @@ int ne_par(Branch*& aBranch)   // для непарних елементів
 	return amount_1;
 }
 
-int summa(Branch*& aBranch) // сума всіх елементів
+int summa(Branch*& aBranch) // suma vsikh elementiv
 {
 	int sum = 0;
 	if (aBranch != NULL) {
@@ -103,7 +103,7 @@ int summa(Branch*& aBranch) // сума всіх елементів
 	}
 	return sum;
 }
-void add_elem(int aData, Branch*& aBranch) // додавання нового елементу
+void add_elem(int aData, Branch*& aBranch) // dodavannya novoho elementu
 {
 	if (!aBranch)
 	{
@@ -124,7 +124,7 @@ void add_elem(int aData, Branch*& aBranch) // додавання нового елементу
 	}
 }
 
-void FreeTree(Branch* aBranch) // звільнення пам'яті
+void FreeTree(Branch* aBranch) // zvilnennya pamyati
 {
 	if (!aBranch) return;
 	FreeTree(aBranch->LeftBranch);
@@ -133,7 +133,7 @@ void FreeTree(Branch* aBranch) // звільнення пам'яті
 	return;
 }
 
-Branch* del_elem(Branch*& aBranch, int aData) // видалення нового елементу
+Branch* del_elem(Branch*& aBranch, int aData) // vydalennya novoho elementu
 {
 	if (aBranch == NULL)
 		return aBranch;
@@ -187,7 +187,8 @@ int main()
 
 	for (int i = 0; i < num; i++)
 	{
-		Add(rand() % 50, Root); // заповнення цілими числами від 0 до 50
+		Add(rand() % 50, Root); // zapovnennya tsilymy chyslamy vid 0 do 50
+		//Add(1, Root);
 		
 	}
 	cout << "Вивiд бiнарного дерева: " << endl;
@@ -202,16 +203,17 @@ int main()
 	gor_obh(Root);
 	cout << endl;
 
-	cout << "Додавання елемента в бiнарне дерево:" << endl;
+	cout << "Додавання нового елементу в бiнарне дерево:" << endl;
 	cout << "Введiть елемет: ";
 	cin >> element;
 	add_elem(element, Root);
+	//Add(element, Root);
 
 	cout << "Вивiд бiнарного дерева: " << endl;
 	print(Root);
 	cout << endl;
 
-	cout << "Видалення елементу з бiнарного дерева:" << endl;
+	cout << "Видалення нвого елементу з бiнарного дерева:" << endl;
 	cout << "Введiть елемет: ";
 	cin >> element;
 	del_elem(Root, element);
